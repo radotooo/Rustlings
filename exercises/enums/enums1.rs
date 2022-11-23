cf5c300b00a77461c1dd014a6c5e5ea735528a08
radotooo <radotooo@gmail.com>
2022-11-23 22:44:37 +0200

    enums

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,14 +1,16 @@
 // enums1.rs
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
 }
 
 fn main() {
+    println!("{:?}", Message::Quit);
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
     println!("{:?}", Message::Move);
