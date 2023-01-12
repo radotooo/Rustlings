7b48fe028febc54adef792d21bba608f35a95c6c
radotooo <radotooo@gmail.com>
2023-01-12 23:23:51 +0200

    from_into, using_as

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let default = Person::default();
+        let words: Vec<&str> = s.split(',').collect();
+
+        if s.len() == 0 || words.len() != 2 || words[0].is_empty() {
+            return default;
+        } else {
+            let name = words[0].to_string();
+            let age = words[1].parse::<usize>();
+
+            if let Err(age) = age {
+                return default;
+            }
+
+            return Person {
+                name,
+                age: age.unwrap(),
+            };
+        }
     }
 }
 
